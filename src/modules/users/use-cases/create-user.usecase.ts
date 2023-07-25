7aa56422b9c90357a66d9221c4013c4dd7ff29ca
Victor Caio <kaioaraujo426@gmail.com>
2023-07-25 01:44:52 -0300

    FEAT: added password encryption

--- src/modules/users/use-cases/create-user.usecase.ts
+++ src/modules/users/use-cases/create-user.usecase.ts
@@ -1,5 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import * as bcrypt from 'bcrypt';
+
 import { PrismaService } from 'src/infra/database/prisma/prisma.service';
 import { CreateUserDTO } from '../dto/create-user.dto';
 
@@ -17,6 +19,8 @@ export class CreateUserUseCase {
 
     if (name === undefined || password === undefined)
       throw new Error('Invalid fields!');
+
+    password = await bcrypt.hash(password, 6);
 
     const userCreated = await this.prismaService.users.create({
       data: { name, email, password, username, id: randomUUID() },
