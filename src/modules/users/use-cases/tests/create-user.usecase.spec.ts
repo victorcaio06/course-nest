8dcc20269ab537f7ca46fa1514ca5167069334ab
Victor Caio <kaioaraujo426@gmail.com>
2023-08-07 01:48:01 -0300

    TEST: Test implemented to verify that it is not possible to create a new user with an existing username

--- src/modules/users/use-cases/tests/create-user.usecase.spec.ts
+++ src/modules/users/use-cases/tests/create-user.usecase.spec.ts
@@ -3,6 +3,7 @@ import { CreateUserUseCase } from '../create-user.usecase';
 import { CreateUserDTO } from '../../dto/user.dto';
 import { IUserRepository } from '../../repositories/user.repository';
 import { UserInMemoryRepository } from '../../repositories/in-memory/user.in-memory.repository';
+import { HttpException } from '@nestjs/common';
 
 describe('Create user', () => {
   let createUserUseCase: CreateUserUseCase;
@@ -32,5 +33,27 @@ describe('Create user', () => {
     const result = await createUserUseCase.execute(data);
 
     expect(result).toHaveProperty('created_at');
+  });
+
+  it('should not be able to create a new user if username already exists', async () => {
+    const data: CreateUserDTO = {
+      name: 'Regina',
+      username: 'regina123',
+      email: 'reginaGatinha@gmail.com',
+      password: '123456',
+    };
+
+    const dataExistingUsername: CreateUserDTO = {
+      name: 'Regina',
+      username: 'regina123',
+      email: 'regina123@gmail.com',
+      password: '123456',
+    };
+
+    await createUserUseCase.execute(data);
+
+    expect(async () => {
+      await createUserUseCase.execute(dataExistingUsername);
+    }).rejects.toThrowError(HttpException);
   });
 });
